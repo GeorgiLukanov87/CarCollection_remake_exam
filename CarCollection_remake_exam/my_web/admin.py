12fc7531a404dd7b51ab6f6b79a0cb8874e5ca9a
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-21 20:01:23 +0200

    car model

--- CarCollection_remake_exam/my_web/admin.py
+++ CarCollection_remake_exam/my_web/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from CarCollection_remake_exam.my_web.models import Profile
+from CarCollection_remake_exam.my_web.models import Profile, Car
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'age',)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'model', 'year',)
--- CarCollection_remake_exam/my_web/models.py
+++ CarCollection_remake_exam/my_web/models.py
@@ -1,5 +1,11 @@
 from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def validate_car_year(value):
+    if value <= 1980 or value >= 2049:
+        raise ValidationError('Year must be between 1980 and 2049!')
 
 
 class Profile(models.Model):
@@ -36,4 +42,38 @@ class Profile(models.Model):
     profile_picture = models.URLField(
         blank=True,
         null=True,
+    )
+
+
+class Car(models.Model):
+    CAR_TYPE_CHOICES = (
+        ('Sports Car', '1-Sports Car'),
+        ('Pickup', '2-Pickup'),
+        ('Crossover', '3-Crossover'),
+        ('Minibus', '4-Minibus'),
+        ('Other', '5-Other'),
+    )
+
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPE_CHOICES,
+    )
+
+    model = models.CharField(
+        max_length=20,
+        validators=[
+            validators.MinLengthValidator(2, message='It should consist of a minimum of 2 characters!')
+        ],
+    )
+
+    year = models.PositiveIntegerField(
+        validators=[
+            validate_car_year,
+        ]
+    )
+
+    image_url = models.URLField()
+
+    price = models.FloatField(
+        validators=[validators.MinValueValidator(1, message='Price cannot be below 1!')]
     )
