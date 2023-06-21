a31efcf79237fa0f85a04285955e44d934f4001e
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-21 19:44:26 +0200

    templates order and views create

--- CarCollection_remake_exam/my_web/urls.py
+++ CarCollection_remake_exam/my_web/urls.py
@@ -1,1 +1,40 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from CarCollection_remake_exam.my_web.views import index, catalogue, create_car, details_car, edit_car, delete_car, \
+    create_profile, edit_profile, delete_profile, details_profile
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('catalogue/', catalogue, name='catalogue'),
+
+    path('car/', include([
+        path('create/', create_car, name='create-car'),
+        path('<int:pk>/details/', details_car, name='details-car'),
+        path('<int:pk>/edit/', edit_car, name='edit-car'),
+        path('<int:pk>/delete/', delete_car, name='delete-car'),
+    ])),
+
+    path('profile/', include([
+        path('create/', create_profile, name='create-profile'),
+        path('edit/', edit_profile, name='edit-profile'),
+        path('delete/', delete_profile, name='delete-profile'),
+        path('details/', details_profile, name='details-profile'),
+    ]))
+)
+
+"""
+
+http://localhost:8000/ - index page
+http://localhost:8000/catalogue/ - catalogue page
+
+http://localhost:8000/car/create/ - car create page
+http://localhost:8000/car/1/details/ - car details page
+http://localhost:8000/car/1/edit/ - car edit page
+http://localhost:8000/car/1/delete/ - car delete page
+
+http://localhost:8000/profile/create - profile create page
+http://localhost:8000/profile/details/ - profile details page
+http://localhost:8000/profile/edit/ - profile edit page
+http://localhost:8000/profile/delete/ - profile delete page
+
+"""
--- CarCollection_remake_exam/my_web/views.py
+++ CarCollection_remake_exam/my_web/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'common/index.html')
+
+
+def catalogue(request):
+    return render(request, 'common/catalogue.html')
+
+
+def create_car(request):
+    return render(request, 'car/car-create.html')
+
+
+def details_car(request, pk):
+    return render(request, 'car/car-details.html')
+
+
+def edit_car(request, pk):
+    return render(request, 'car/car-edit.html')
+
+
+def delete_car(request, pk):
+    return render(request, 'car/car-delete.html')
+
+
+def create_profile(request):
+    return render(request, 'profile/profile-create.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/profile-edit.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/profile-delete.html')
+
+
+def details_profile(request):
+    return render(request, 'profile/profile-details.html')
